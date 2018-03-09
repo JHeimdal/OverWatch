7d047fe4ecb289696148304e04f613681c46d65f
Jimmy Heimdal <jimmy@localhost.localdomain>
2018-03-09 20:57:24 +0100

    Still laying the ground work

--- overwatch.py
+++ overwatch.py
@@ -3,7 +3,8 @@
 import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QWidget, QFrame,QLabel,QPushButton,
-    QHBoxLayout,QStyleFactory,QSplitter,QTextEdit,QGridLayout,QLCDNumber,QDial)
+    QHBoxLayout,QStyleFactory,QSplitter,QTextEdit,QGridLayout,QLCDNumber,QDial,
+    QProgressBar)
 from pyqtgraph import PlotWidget
 
 class OverWatch(QWidget):
@@ -18,22 +19,32 @@ class OverWatch(QWidget):
         tH = QFrame(self)
         tH.setFrameShape(QFrame.StyledPanel)
         tHvbox = QGridLayout()
-        tHvbox.addWidget(QLabel('Out Temp'),1,0)
-        tHvbox.addWidget(QLabel('In Temp') ,1,1)
+        tHvbox.addWidget(QLabel('Inside'),1,1,1,2)
+        tHvbox.addWidget(QLabel('Outside') ,1,3,1,2)
+        tHvbox.addWidget(QLabel('Temp:'),2,0)
+        tHvbox.addWidget(QLabel('C') ,2,2)
+        tHvbox.addWidget(QLabel('C') ,2,4)
+        tHvbox.addWidget(QLabel('Humidity:'),3,0)
+        tHvbox.addWidget(QLabel('%') ,3,2)
+        tHvbox.addWidget(QLabel('%') ,3,4)
         self.outTemp=QLCDNumber()
         self.inTemp=QLCDNumber()
-        tHvbox.addWidget(self.outTemp,2,0)
-        tHvbox.addWidget(self.inTemp ,2,1)
+        self.outHD=QLCDNumber()
+        self.inHD=QLCDNumber()
+        tHvbox.addWidget(self.outTemp,2,3,1,1)
+        tHvbox.addWidget(self.inTemp ,2,1,1,1)
+        tHvbox.addWidget(self.outHD,3,3,1,1)
+        tHvbox.addWidget(self.inHD ,3,1,1,1)
         self.tPlot = PlotWidget()
-        tHvbox.addWidget(self.tPlot, 3,0,2,2)
+        tHvbox.addWidget(self.tPlot, 4,0,4,5)
         self.weekbt = QPushButton('Week')
         self.monthbt = QPushButton('Month')
-        tHvbox.addWidget(self.weekbt, 5,0)
-        tHvbox.addWidget(self.monthbt,5,1)
-        self.windDir = QDial()
-        self.windDir.setWrapping(True)
-        self.windDir.setNotchesVisible(True)
-        tHvbox.addWidget(self.windDir,1,2,5,5)
+        tHvbox.addWidget(self.weekbt, 8,0)
+        tHvbox.addWidget(self.monthbt,8,1)
+        self.pBar = QProgressBar()
+        self.pBar.setOrientation(Qt.Vertical)
+        tHvbox.addWidget(self.pBar,4,6)
+        self.pBar.setValue(50)
 
         tH.setLayout(tHvbox)
 
@@ -61,4 +72,3 @@ if __name__=='__main__':
     app = QApplication(sys.argv)
     ovw = OverWatch()
     sys.exit(app.exec_())
-    
